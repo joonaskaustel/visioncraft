d26976de1fd214dd823cfe0baf717e2684cdb8bf
joonas <joonas.kaustel@gmail.com>
2020-11-24 22:58:12 +0200

    init

---
+++ api/src/user/dto/register.user.dto.ts
@@ -0,0 +1,17 @@
+import {IsEmail, IsNotEmpty, IsString, Length, MinLength, ValidateIf} from 'class-validator';
+
+export class RegisterUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    password: string;
+
+    @ValidateIf((user) => user.confirmPassword !== user.password)
+    @Length(1, 0, { message: 'The two provided passwords are different!' }) // a bit hacky but should work
+    confirmPassword: string;
+}
